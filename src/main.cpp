aac10cad98e05a63bfdf5a259c186d26d5f75366
theoneandonlydavid <52057976+theoneandonlydavid@users.noreply.github.com>
2024-06-16 23:14:47 -0500

    implemented a main window

--- src/main.cpp
+++ src/main.cpp
@@ -5,8 +5,8 @@ using namespace DirectX;
 // PREAMBLE
 const LONG g_WindowWidth = 1280;
 const LONG g_WindowHeight = 720;
-LPCSTR g_WindowClassName = "DirectXWindowClass";
-LPCSTR g_WindowName = "Echo Engine";
+LPCWSTR g_WindowClassName = L"DirectXWindowClass";
+LPCWSTR g_WindowName = L"Echo Engine";
 HWND g_WindowHandle = 0;
 
 const BOOL g_EnableVSync = TRUE;
@@ -93,4 +93,108 @@ void Render();
 void Cleanup();
 
 // THE MAIN WINDOW
-// TODO: Implement the main window.
+
+// Initialize the application window.
+int InitApplication(HINSTANCE hInstance, int cmdShow) {
+    WNDCLASSEX wndClass = { 0 };
+    wndClass.cbSize = sizeof(WNDCLASSEX);
+    wndClass.style = CS_HREDRAW | CS_VREDRAW;
+    wndClass.lpfnWndProc = &WndProc;
+    wndClass.hInstance = hInstance;
+    wndClass.hCursor = LoadCursor(nullptr, IDC_ARROW);
+    wndClass.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
+    wndClass.lpszMenuName = nullptr;
+    wndClass.lpszClassName = g_WindowClassName;
+
+    if (!RegisterClassEx(&wndClass)) {
+        return -1;
+    }
+
+    RECT windowRect = { 0, 0, g_WindowWidth, g_WindowHeight };
+    AdjustWindowRect(&windowRect, WS_OVERLAPPEDWINDOW, FALSE);
+
+    g_WindowHandle = CreateWindow(g_WindowClassName, g_WindowName, WS_OVERLAPPEDWINDOW, CW_USEDEFAULT, CW_USEDEFAULT, windowRect.right - windowRect.left, windowRect.bottom - windowRect.top, nullptr, nullptr, hInstance, nullptr);
+
+    if (!g_WindowHandle) {
+        return -1;
+    }
+
+    ShowWindow(g_WindowHandle, cmdShow);
+    UpdateWindow(g_WindowHandle);
+
+    return 0;
+}
+
+LRESULT CALLBACK WndProc(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam) {
+    PAINTSTRUCT paintStruct;
+    HDC hDC;
+
+    switch (message) {
+    case WM_PAINT:
+        {
+            hDC = BeginPaint(hwnd, &paintStruct);
+            EndPaint(hwnd, &paintStruct);
+        }
+        break;
+    case WM_DESTROY: 
+        { 
+            PostQuitMessage(0);
+        }
+        break;
+    default:
+        return DefWindowProc(hwnd, message, wParam, lParam);
+    }
+
+    return 0;
+}
+
+// The main application loop.
+int Run() {
+    MSG msg = { 0 };
+
+    static DWORD previousTime = timeGetTime();
+    static const float targetFramerate = 30.0f;
+    static const float maxTimeStep = 1.0f / targetFramerate;
+
+    while (msg.message != WM_QUIT) {
+        if (PeekMessage(&msg, 0, 0, 0, PM_REMOVE)) {
+            TranslateMessage(&msg);
+            DispatchMessage(&msg);
+        }
+        else {
+            DWORD currentTime = timeGetTime();
+            float deltaTime = (currentTime - previousTime) / 1000.0f;
+            previousTime = currentTime;
+
+            /* Cap the deltat time to the max time step (useful for debugging and you dont want
+            the deltaTime value to explode.*/
+            deltaTime = std::min<float>(deltaTime, maxTimeStep);
+
+            //Update(deltaTime);
+            //Render();
+        }
+    }
+    return static_cast<int>(msg.wParam);
+}
+
+int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE prevInstance, LPWSTR cmdLine, int cmdShow) {
+    UNREFERENCED_PARAMETER(prevInstance);
+    UNREFERENCED_PARAMETER(cmdLine);
+
+    //Check for DirectX Math library support.
+
+    if (!XMVerifyCPUSupport()) {
+        MessageBox(nullptr, TEXT("Failed to verify DirectX Math library support."), TEXT("Error"), MB_OK);
+        return -1;
+    }
+    if (InitApplication(hInstance, cmdShow) != 0) {
+        MessageBox(nullptr, TEXT("Failed to create application window."), TEXT("Error"), MB_OK);
+        return -1;
+    }
+
+    int returnCode = Run();
+
+    return returnCode;
+}
+
+//TODO: Initialize DirectX 11.
